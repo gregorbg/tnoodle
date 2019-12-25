d47af7c95192c9afcb84ca1910eb7e2655f2ad8a
Gregor Billing <suushiemaniac@gmail.com>
2019-12-25 17:54:13 +0100

    Remove unused itext5 reference from buildSrc

--- buildSrc/src/main/kotlin/dependencies/Libraries.kt
+++ buildSrc/src/main/kotlin/dependencies/Libraries.kt
@@ -3,7 +3,6 @@ package dependencies
 object Libraries {
     val MARKDOWNJ_CORE = "org.markdownj:markdownj-core:${Versions.MARKDOWNJ_CORE}"
     val ZIP4J = "net.lingala.zip4j:zip4j:${Versions.ZIP4J}"
-    val ITEXTPDF = "com.itextpdf:itextpdf:${Versions.ITEXTPDF}"
     val ITEXT_7_KERNEL = "com.itextpdf:kernel:${Versions.ITEXT_7_KERNEL}"
     val ITEXT_7_IO = "com.itextpdf:io:${Versions.ITEXT_7_IO}"
     val ITEXT_7_LAYOUT = "com.itextpdf:layout:${Versions.ITEXT_7_LAYOUT}"
