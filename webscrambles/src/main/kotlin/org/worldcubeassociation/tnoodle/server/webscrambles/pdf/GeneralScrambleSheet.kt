1a1500c10f8e1ff8d183762254ff19ea265e3196
Gregor Billing <gbilling@worldcubeassociation.org>
2021-03-30 17:45:57 +0200

    Minor refactors for GeneralScrambleSheet

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/pdf/GeneralScrambleSheet.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/pdf/GeneralScrambleSheet.kt
@@ -3,10 +3,11 @@ package org.worldcubeassociation.tnoodle.server.webscrambles.pdf
 import com.itextpdf.kernel.colors.ColorConstants
 import com.itextpdf.kernel.colors.DeviceRgb
 import com.itextpdf.kernel.pdf.PdfDocument
-import com.itextpdf.kernel.pdf.PdfPage
 import com.itextpdf.layout.Document
+import com.itextpdf.layout.borders.Border
 import com.itextpdf.layout.element.*
 import com.itextpdf.layout.property.HorizontalAlignment
+import com.itextpdf.layout.property.TextAlignment
 import com.itextpdf.layout.property.VerticalAlignment
 import com.itextpdf.svg.converter.SvgConverter
 import org.worldcubeassociation.tnoodle.server.webscrambles.pdf.util.FontUtil
@@ -15,15 +16,13 @@ import org.worldcubeassociation.tnoodle.server.webscrambles.wcif.model.Scramble
 import org.worldcubeassociation.tnoodle.server.webscrambles.wcif.model.ScrambleSet
 
 class GeneralScrambleSheet(scrambleSet: ScrambleSet, activityCode: ActivityCode) : BaseScrambleSheet(scrambleSet, activityCode) {
-    override fun PdfDocument.writeContents() = addNewPage().writeScrambleContents()
+    override fun PdfDocument.writeContents() {
+        val pdf = Document(this)
 
-    fun PdfPage.writeScrambleContents() {
-        val foo = Document(document)
-
-        foo.addScrambleTable(scrambleSet.scrambles)
+        pdf.addScrambleTable(scrambleSet.scrambles)
 
         if (scrambleSet.extraScrambles.isNotEmpty()) {
-            foo.addScrambleTable(scrambleSet.extraScrambles, EXTRA_SCRAMBLE_PREFIX, TABLE_HEADING_EXTRA_SCRAMBLES)
+            pdf.addScrambleTable(scrambleSet.extraScrambles, EXTRA_SCRAMBLE_PREFIX, TABLE_HEADING_EXTRA_SCRAMBLES)
         }
     }
 
@@ -33,16 +32,18 @@ class GeneralScrambleSheet(scrambleSet: ScrambleSet, activityCode: ActivityCode)
             .setAutoLayout()
 
         if (specialHeading != null) {
+            val headingPar = Paragraph(specialHeading)
+                .setTextAlignment(TextAlignment.CENTER)
+
             val headingCell = Cell(1, 3)
-                .add(Paragraph(specialHeading))
                 .setVerticalAlignment(VerticalAlignment.MIDDLE)
+                .setBorder(Border.NO_BORDER)
+                .add(headingPar)
 
             table.addCell(headingCell)
         }
 
         val strScrambles = scrambles.toPDFStrings(scramblingPuzzle.shortName)
-
-        val scrambleFont = FontUtil.MONO_FONT
 
         for ((i, scramble) in strScrambles.withIndex()) {
             val indexLabel = Text("$scrambleNumberPrefix${i + 1}")
@@ -55,12 +56,12 @@ class GeneralScrambleSheet(scrambleSet: ScrambleSet, activityCode: ActivityCode)
             table.addCell(nthScramble)
 
             val scramblePhrase = Paragraph(scramble)
-                .setMultipliedLeading(1.1f) // FIXME const
+                .setMultipliedLeading(SCRAMBLE_LINE_LEADING)
             // TODO highlighting, cell renderer
 
             val scrambleCell = Cell().add(scramblePhrase)
                 .setKeepTogether(true)
-                .setFont(scrambleFont)
+                .setFont(SCRAMBLE_FONT)
                 .setVerticalAlignment(VerticalAlignment.MIDDLE)
 
             table.addCell(scrambleCell)
@@ -82,24 +83,14 @@ class GeneralScrambleSheet(scrambleSet: ScrambleSet, activityCode: ActivityCode)
                 .takeUnless { puzzleName == "minx" } // minx scrambles intentionally include "\n" chars for alignment
                 ?: map { it.scrambleString }
 
-        const val MAX_SCRAMBLES_PER_PAGE = 7
-        const val MAX_SCRAMBLE_IMAGE_RATIO = 3
-        const val SCRAMBLE_IMAGE_PADDING = 2
-
-        const val SCRAMBLE_MARGIN = 5
         const val STD_SCRAMBLE_PREFIX = ""
         const val EXTRA_SCRAMBLE_PREFIX = "E"
 
-        const val TABLE_HEADING_EXTRA_SCRAMBLES = "Extra Scrambles"
+        const val TABLE_HEADING_EXTRA_SCRAMBLES = "Extra Scrambles" // TODO Translate
 
+        const val SCRAMBLE_LINE_LEADING = 1.1f
+
+        private val SCRAMBLE_FONT = FontUtil.MONO_FONT
         private val HIGHLIGHT_COLOR = DeviceRgb(230, 230, 230)
-
-        const val INDEX_COLUMN_WIDTH_RATIO = 25
-        const val EXTRA_SCRAMBLES_HEIGHT_RATIO = 30
-
-        const val VERTICAL_MARGIN = 15f
-        const val HORIZONTAL_MARGIN = 35f
-
-        const val NEW_LINE = "\n"
     }
 }
